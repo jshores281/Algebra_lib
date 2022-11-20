e9fadebdcf866ef476e42446e8973e541eb09d52
jshores281 <52839097+jshores281@users.noreply.github.com>
2022-11-19 19:39:33 -0600

    cleaned up functions. rem prompts for iteration use of functions

--- algebra.py
+++ algebra.py
@@ -1,65 +1,79 @@
 
 
 
-
-
-class algebra:
-	def add(x, y):
+class functions:
+	def calc_logistic(t, l, y=1, k=0):
 		"""
-		Simple addition test
-		"""
-
-		outcome = x + y
-		return outcome
-
-
-	def roc_logi(t1, t2):
-		"""
-
-		Allows the calculations of the rate of change between two 
-		points of time in a logistics function. 
 		
-		The roc_logi(t1, t2) is used for known points of time. 
-		   t1 = first point, t2 = second point. 
+		calc_logistic(t, L, Y, K) 
+
+		Calculates the rate of change between one
+ 		point in a logistics function. 
+
 
 		function:
 			   L
-		f(x) =  --------------
-	               1 + 1e^-k(x - x0)
+		f(t) =  --------------
+	               1 + y*e^-k(x-x0)
 
+		t = points of time
 		L = supremom or curves maximum value
 		k = logistic growth rate i.e steepness of curve
 		e = eulers number used as constant
+		y = eulers multiplier. defaults to one if none
 		x = sigmoid midpoint
 
+		"""
+
+		e = float(2.718281828459045)
+
+		y1 = l/(1+y*e**(k*t))
+		y1 = round(y1, 6)
+
+		return [t, y1]
+
+
+	def roc_logistic(t1, t2, l, y=1, k=0):
+		"""
+
+		roc_logistic(t1, t2, L, Y=1, K=0)  
+		   
+		Calculates average rate of change between two
+ 		points in a logistics function. 
 		
-		These variables are prompted after time is added to roc_logi parameters.
+
+		function:
+			   L
+		f(t) =  --------------
+	               1 + y*e^-k(x-x0)
+
+		t = points of time
+		L = supremom or curves maximum value
+		k = logistic growth rate i.e steepness of curve
+		e = eulers number used as constant
+		y = eulers multiplier. defaults to one if none
+		x = sigmoid midpoint
 
 		"""
-		l = float(input("ENTER max curve:  "))
-		y = float(input("ENTER euler's multiplier, 1 if none: "))
-		k = float(input("ENTER curves midpoint: "))
-		e = float(2.71828)
 
-		y1 = l/(1+y*2.71828**(-k*t1))
-		y2 = l/(1+y*2.71828**(-k*t2))
+		e = float(2.718281828459045)
+
+		y1 = l/(1+y*e**(k*t1))
+		y2 = l/(1+y*e**(k*t2))
+
+		y1 = round(y1, 6)
+		y2 = round(y2, 6)
+
+		print("\nPoint of {} = {}".format(t1, y1))
+		print("Point of {} = {}".format(t2, y2))
 
 		y_out = y2 - y1
 		x_out = t2 - t1
 
+
+
 		outcome = y_out / x_out
 
-		return outcome
-
-
-
-
-
-
-
-
-
-
-
+		return [t1,t2,outcome]
 
 
