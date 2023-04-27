50a516ebe5b99f469767f810a2e5c4fd8936ad7c
jshores281 <52839097+jshores281@users.noreply.github.com>
2023-04-27 13:34:33 -0500

    test api keys push

--- algebra.py
+++ algebra.py
@@ -118,7 +118,7 @@ class spread_metrics:
 			lst += i
 
 		mean = lst/len(a)
-		return f'original list: {a}\nlist length: {alen}\nmean/avg of list: {mean}'
+		return print(f'\noriginal list: {a}\nlist length: {alen}\nmean/avg of list: {mean}')
 
 	# a = [1,2,8,4,5,6,7,3,13,60,34,12,33,45]
 	# print(mean(a))
@@ -169,8 +169,5 @@ class spread_metrics:
 
 	def std_devi():
 		pass
-
-
-
 
 
